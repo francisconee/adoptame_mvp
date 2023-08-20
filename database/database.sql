cf2c9dad28e88669b88928134020a68279050f41
francisconee <francisco.loy@gmail.com>
2023-08-20 03:52:51 -0400

    avances de maqueta

--- database/database.sql
+++ database/database.sql
@@ -6,6 +6,7 @@ CREATE TABLE mascotasv2 (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(180),
     description VARCHAR(255),
+    minidescription VARCHAR(200),
     image VARCHAR(200),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
